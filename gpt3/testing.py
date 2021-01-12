c3ba133a791c94d35c90f89c92deb0c78ace3e74
Srikar Valluri <srikarvalluri@Srikars-MacBook-Pro.local>
2021-01-12 15:48:23 -0800

    everything's working

--- gpt3/testing.py
+++ gpt3/testing.py
@@ -3,21 +3,33 @@ import json
 import nltk
 from textblob import TextBlob
 import openai
+import pickle
+
+from gpt3 import GPT
+from gpt3 import Example
 
 
-gpt = pickle.load(gpt, open( "../models/gpt3_model.p", "rb" ))
+nltk.download('brown')
+openai.api_key = "sk-bevCcAUQKr16OtmKeFgN93C0uSOGMHQH9Q50ohAs"
 
-prompt1 = ["trump", "impeachment", "capitol", "america", "funny"]
-prompt2 = ["game of thrones", "trump", "funny"]
-prompt3 = ["epic fortnite gamer", "apex legends", "ban for racism", "funny"]
-prompt4 = ["meme", "bruh moment", "artificial intelligence", "funny"]
+gpt = pickle.load(open( "../models/gpt3_model.p", "rb" ))
 
-output1 = gpt.submit_request(prompt1)
-output2 = gpt.submit_request(prompt2)
-output3 = gpt.submit_request(prompt3)
-output4 = gpt.submit_request(prompt4)
+# prompt1 = '["trump", "impeachment", "capitol", "america", "funny"]'
+# prompt2 = '["game of thrones", "trump", "funny"]'
+# prompt3 = '["epic fortnite gamer", "apex legends", "ban for racism", "funny"]'
+# prompt4 = '["meme", "bruh moment", "artificial intelligence", "funny"]'
+prompt5 = '["insurrectionist", "dab on the haters", "lemons", "funny"]'
 
-print(output1.choices[0].text)
-print(output2.choices[0].text)
-print(output3.choices[0].text)
-print(output4.choices[0].text)
+
+# output1 = gpt.submit_request(prompt1)
+# output2 = gpt.submit_request(prompt2)
+# output3 = gpt.submit_request(prompt3)
+# output4 = gpt.submit_request(prompt4)
+output5 = gpt.submit_request(prompt5)
+
+
+# print(output1.choices[0].text)
+# print(output2.choices[0].text)
+# print(output3.choices[0].text)
+# print(output4.choices[0].text)
+print(output5.choices[0].text)
--- gpt3/training.py
+++ gpt3/training.py
@@ -3,26 +3,30 @@ import json
 import nltk
 from textblob import TextBlob
 import openai
-
+import pickle
 
 nltk.download('brown')
 openai.api_key = "sk-bevCcAUQKr16OtmKeFgN93C0uSOGMHQH9Q50ohAs"
 
-from gpt import GPT
-from gpt import Example
+from gpt3 import GPT
+from gpt3 import Example
 
-token_limit = 5000
+token_limit = 100
 
 gpt_model = GPT(engine='davinci', temperature=0.5, max_tokens=token_limit)
 
 
 with open('../data/sample_text.txt') as text_file:
+    i = 0
     for line in text_file:
         blob = TextBlob(line)
         num_of_words = random.randint(1,4)
         input_data = blob.noun_phrases[0:num_of_words]
         input_data.append("funny")
+        input_data = str(input_data)
 
         gpt_model.add_example(Example(input_data, line))
-        pickle.dump(gpt, open( "../models/gpt3_model.p", "wb" ))
+        pickle.dump(gpt_model, open( "../models/gpt3_model.p", "wb" ))
+        print("This is the " + str(i) + " iteration.")
+        i += 1
 
